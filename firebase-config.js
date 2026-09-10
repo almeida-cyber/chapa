66f3426f2ee0c5b0316d47fcb4d5885e7acfd982
chapa <joaopenhap4@gmail.com>
2026-09-10 02:56:40 -0300

    Change WhatsApp number format in firebase-config.js
    Updated WhatsApp number format for consistency.

--- firebase-config.js
+++ firebase-config.js
@@ -17,7 +17,7 @@ const CONFIG = {
     nome: "Comida na Chapa",
     descricao: "Quentinha, saborosa e feita na hora"
   },
-  whatsappNumber: "96984352841",
+  whatsappNumber: "5596984352841",
   pixKey: "96984352841",
   produtos: [
     { id: "calabresa", nome: "Marmita de Calabresa", descricao: "Calabresa na chapa com acompanhamento.", preco: 25, imagem: "marmita de calabresa.png", disponivel: true },
